00872e1bc8843edce590e29eb5e842f86fa799fa
Steve Clay <steve@mrclay.org>
2020-03-25 22:40:07 -0400

    Prettier

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const fetch = require('node-fetch');
-const fs = require('fs');
-const qs = require('querystring');
+const fetch = require("node-fetch");
+const fs = require("fs");
+const qs = require("querystring");
 
-const ROOT = 'https://api.buildkite.com/v2';
+const ROOT = "https://api.buildkite.com/v2";
 
 async function get(url, query = null) {
-  const fullUrl = url + (query ? `?${qs.stringify(query)}` : '');
+  const fullUrl = url + (query ? `?${qs.stringify(query)}` : "");
   console.info(`Fetching: ${fullUrl}`);
   const res = await fetch(fullUrl, {
-    headers: {'Authorization': `Bearer ${process.env.BK_TOKEN}`},
+    headers: { Authorization: `Bearer ${process.env.BK_TOKEN}` },
   });
   return res.json();
 }
@@ -37,55 +37,63 @@ async function go() {
   const allPipelines = [];
   const longBuilds = [];
 
-  await pagedGet(`${ROOT}/organizations/${process.env.BK_ORG}/pipelines`, {}, pipelines => {
-    for (const pipeline of pipelines) {
-      const { slug, web_url, steps, provider, name } = pipeline;
-      const repo = 'https://github.com/' + provider.settings.repository;
-      if (!steps.length) {
-        continue;
-      }
+  await pagedGet(
+    `${ROOT}/organizations/${process.env.BK_ORG}/pipelines`,
+    {},
+    (pipelines) => {
+      for (const pipeline of pipelines) {
+        const { slug, web_url, steps, provider, name } = pipeline;
+        const repo = "https://github.com/" + provider.settings.repository;
+        if (!steps.length) {
+          continue;
+        }
 
-      const uses_yaml = steps[0].command === 'buildkite-agent pipeline upload';
-      if (!uses_yaml) {
-        allPipelines.push({
-          slug,
-          name: name.trim(),
-          web_url,
-          repo,
-        });
-      }
-    }
-  });
-
-  await pagedGet(`${ROOT}/organizations/${process.env.BK_ORG}/builds`, { state: 'running' }, builds => {
-    for (const build of builds) {
-      const { web_url: build_url, creator, started_at } = build;
-      const elapsed = new Date().getTime() - new Date(started_at).getTime();
-
-      if (elapsed > process.env.MINUTES * 60e3) {
-        longBuilds.push({
-          build_url,
-          email: creator.email,
-          name: creator.name,
-        });
+        const uses_yaml =
+          steps[0].command === "buildkite-agent pipeline upload";
+        if (!uses_yaml) {
+          allPipelines.push({
+            slug,
+            name: name.trim(),
+            web_url,
+            repo,
+          });
+        }
       }
     }
-  });
+  );
 
-  let md = '';
+  await pagedGet(
+    `${ROOT}/organizations/${process.env.BK_ORG}/builds`,
+    { state: "running" },
+    (builds) => {
+      for (const build of builds) {
+        const { web_url: build_url, creator, started_at } = build;
+        const elapsed = new Date().getTime() - new Date(started_at).getTime();
+
+        if (elapsed > process.env.MINUTES * 60e3) {
+          longBuilds.push({
+            build_url,
+            email: creator.email,
+            name: creator.name,
+          });
+        }
+      }
+    }
+  );
+
+  let md = "";
 
   md += `\n## long-running builds\n\n`;
-  longBuilds.forEach(b => {
+  longBuilds.forEach((b) => {
     md += `* ${b.build_url} ([${b.name}](mailto:${b.email}))\n`;
   });
 
-  md += '\n## unconverted repos\n\n';
-  allPipelines.forEach(p => {
+  md += "\n## unconverted repos\n\n";
+  allPipelines.forEach((p) => {
     md += `* **[${p.name}](${p.repo})** - [pipeline](${p.web_url})\n`;
   });
 
   fs.writeFileSync(`${__dirname}/${process.env.OUT}`, md);
 }
 
-go()
-  .catch(e => console.error(e));
+go().catch((e) => console.error(e));
